c1ec5cf072fd49de51f6f673e754234cdf139daf
hellojinwoo <hellojinwoo@snu.ac.kr>
2023-04-28 22:57:19 +0200

    Deployed 3b477d1 with MkDocs version: 1.4.2

---
+++ adsense.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2773617129169224';
+  script.setAttribute('crossorigin', 'anonymous');
+  document.head.appendChild(script);
+});
